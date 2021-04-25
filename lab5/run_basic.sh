03ebde585e1351851fca5e187f26bbd47ccfab6d
msokol <msokol@vnode-01.dydaktyka.icsr.agh.edu.pl>
2021-04-25 23:40:26 +0200

    Gather logs from basic program execution

---
+++ lab5/run_basic.sh
@@ -0,0 +1,15 @@
+# SET POINTS RANGE
+POINTS_QUANTITY_MAX=128000000
+POINTS_QUANTITY_START=1000000
+
+# GATHER LOGS
+buffer_size=$POINTS_QUANTITY_START
+while [ $buffer_size -le $POINTS_QUANTITY_MAX ]
+do
+    for i in {1..10}
+    do
+        ./bucket_sort.out $buffer_size >> logs/basic_points_$buffer_size.log
+    done
+    buffer_size=$(($buffer_size * 2))
+done
+
