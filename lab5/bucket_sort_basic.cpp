9087d57fa577e443f19a5d1c4284fa06b69cdad3
mlokos <mlokos.github@gmail.com>
2021-04-26 00:21:36 +0200

    Split code into explicit basic-parallel invocation

---
+++ lab5/bucket_sort_basic.cpp
@@ -0,0 +1,42 @@
+/**
+ * AUTHORS:
+ * Ola Pasternak
+ * Michal Sokol
+ */
+
+#include <iostream> // cout
+#include <random>   // uniform_real_distribution
+#include <vector>   // vector
+#include <chrono>   // time
+#include <omp.h>
+
+#define LLONG_UINT unsigned long long int
+
+
+int main(int argc, char *argv[]) {
+    /**
+    * Arguments handle
+    */
+    if(argc != 2) {
+        printf("Run program with a points quantity argument!");
+        exit(1);
+    }
+    char *ptr;
+    LLONG_UINT vector_size = strtol(argv[1], &ptr, 10);
+
+    std::vector<float> to_fill_vector(vector_size);
+    std::mt19937 gen;
+    std::uniform_real_distribution<float> prob(0.0, 1.0);
+
+    auto start_time = std::chrono::high_resolution_clock::now();
+
+    for(LLONG_UINT i=0; i < to_fill_vector.size(); ++i) {
+        to_fill_vector[i] = prob(gen);
+    }
+    auto end_time = std::chrono::high_resolution_clock::now();
+
+    auto duration = std::chrono::duration_cast<std::chrono::microseconds>(end_time - start_time);
+    std::cout << duration.count() << std::endl;
+
+    return 0;
+ }
--- lab5/run_basic.sh
+++ lab5/run_basic.sh
@@ -8,7 +8,7 @@ while [ $buffer_size -le $POINTS_QUANTITY_MAX ]
 do
     for i in {1..10}
     do
-        ./bucket_sort.out $buffer_size >> logs/basic_points_$buffer_size.log
+        ./bucket_sort_basic.out $buffer_size >> logs/basic_points_$buffer_size.log
     done
     buffer_size=$(($buffer_size * 2))
 done
