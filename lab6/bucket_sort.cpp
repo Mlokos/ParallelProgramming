78d07e7b6102c546c201de5c9576880f95d448cc
Michal Sokol <mlokos.github@gmail.com>
2021-05-04 18:41:34 +0200

    Add openmp to bucket sort

--- lab6/bucket_sort.cpp
+++ lab6/bucket_sort.cpp
@@ -14,14 +14,6 @@
 #define LLONG_UINT unsigned long long int
 
 
-void generate_table(std::vector<float> to_fill_vector, std::mt19937 gen, std::uniform_real_distribution<float> prob) {
-    #pragma omp parallel for shared(to_fill_vector) schedule(static, 8)
-    for(LLONG_UINT i=0; i < to_fill_vector.size(); ++i) {
-        to_fill_vector[i] = prob(gen);
-    }
-}
-
-
 // source: https://www.geeksforgeeks.org/bucket-sort-2/
 // Function to sort arr[] of
 // size n using bucket sort
@@ -32,7 +24,7 @@ std::vector<float> bucketSort(std::vector<float> arr, int n)
  
     // 2) Put array elements
     // in different buckets
-    #pragma omp parallel for schedule(static, 8)
+    #pragma omp parallel for schedule(runtime)
     for (LLONG_UINT i = 0; i < arr.size(); ++i) {
         int bi = n * arr[i]; // Index in bucket
         #pragma omp critical
@@ -40,16 +32,34 @@ std::vector<float> bucketSort(std::vector<float> arr, int n)
     }
  
     // 3) Sort individual buckets
-    #pragma omp parallel for schedule(static, 8)
-    for (int i = 0; i < n; i++)
-        std::sort(b[i].begin(), b[i].end());
- 
+    #pragma omp parallel
+    {
+        std::sort(b[omp_get_thread_num()].begin(), b[omp_get_thread_num()].end());
+    }
+
     // 4) Concatenate all buckets into arr[]
-    int index = 0;
-    #pragma omp parallel for schedule(static, 8)
-    for (int i = 0; i < n; i++)
-        for (int j = 0; j < b[i].size(); j++)
-            arr[index++] = b[i][j];
+    std::vector<LLONG_UINT> bucket_sizes;
+    for(int i = 0; i < n; ++i) {
+        if(i > 0) {
+            bucket_sizes.push_back(b[i].size() + bucket_sizes.back());
+        } else {
+            bucket_sizes.push_back(b[i].size());
+        }
+    }
+    bucket_sizes.insert(bucket_sizes.begin(), 0);
+    
+    #pragma omp parallel
+    {
+        for(int i = 0; i < n; ++i){
+            if(omp_get_thread_num() == i) {
+                for (int j = bucket_sizes[i]; j < bucket_sizes[i]; ++j) {
+                    for (int a = 0; a < b[i].size(); ++a) {
+                        arr[j + a] = b[i][a];
+                    }
+                }
+            }
+        }
+    }
 
     return arr;
 }
@@ -70,10 +80,16 @@ int main(int argc, char *argv[]) {
     std::mt19937 gen;
     std::uniform_real_distribution<float> prob(0.0, 1.0);
 
-    // generate_table(to_fill_vector, gen, prob);
-    #pragma omp parallel for shared(to_fill_vector) schedule(static, 8)
+    #pragma omp parallel for shared(to_fill_vector) schedule(runtime)
     for(LLONG_UINT i=0; i < to_fill_vector.size(); ++i) {
         to_fill_vector[i] = prob(gen);
+    }
+
+    int available_threads;
+    #pragma omp parallel
+    {
+        if(omp_get_thread_num() == 0)
+            available_threads = omp_get_num_threads();
     }
 
     auto start_time = std::chrono::high_resolution_clock::now();
