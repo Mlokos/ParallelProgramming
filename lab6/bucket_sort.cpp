14c7701ac5b14d85086c4baba9b5f4935b57e80b
Michal Sokol <mlokos.github@gmail.com>
2021-05-04 21:23:41 +0200

    Add units to logs

--- lab6/bucket_sort.cpp
+++ lab6/bucket_sort.cpp
@@ -50,9 +50,9 @@ std::vector<float> bucketSort_0(std::vector<float> arr, int n)
     auto bucket_sort_duration = std::chrono::duration_cast<std::chrono::microseconds>(bucket_sort_end - bucket_sort_start);
     auto bucket_to_array_duration = std::chrono::duration_cast<std::chrono::microseconds>(bucket_to_array_end - bucket_to_array_start);
     
-    std::cout << "bucket_split_time " << bucket_split_duration.count() << std::endl;
-    std::cout << "bucket_sort_time " << bucket_sort_duration.count() << std::endl;
-    std::cout << "bucket_to_array_time " << bucket_to_array_duration.count() << std::endl;
+    std::cout << "bucket_split_time " << (double)bucket_split_duration.count()/1000000 << " [s]"  << std::endl;
+    std::cout << "bucket_sort_time " << (double)bucket_sort_duration.count()/1000000 << " [s]"  << std::endl;
+    std::cout << "bucket_to_array_time " << (double)bucket_to_array_duration.count()/1000000 << " [s]"  << std::endl;
 
     return arr;
 }
@@ -114,9 +114,9 @@ std::vector<float> bucketSort_2(std::vector<float> arr, int n)
     auto bucket_sort_duration = std::chrono::duration_cast<std::chrono::microseconds>(bucket_sort_end - bucket_sort_start);
     auto bucket_to_array_duration = std::chrono::duration_cast<std::chrono::microseconds>(bucket_to_array_end - bucket_to_array_start);
     
-    std::cout << "bucket_split_time " << bucket_split_duration.count() << std::endl;
-    std::cout << "bucket_sort_time " << bucket_sort_duration.count() << std::endl;
-    std::cout << "bucket_to_array_time " << bucket_to_array_duration.count() << std::endl;
+    std::cout << "bucket_split_time " << (double)bucket_split_duration.count()/1000000 << " [s]"  << std::endl;
+    std::cout << "bucket_sort_time " << (double)bucket_sort_duration.count()/1000000 << " [s]"  << std::endl;
+    std::cout << "bucket_to_array_time " << (double)bucket_to_array_duration.count()/1000000 << " [s]"  << std::endl;
 
     return arr;
 }
@@ -170,8 +170,8 @@ int main(int argc, char *argv[]) {
     auto fill_table_duration = std::chrono::duration_cast<std::chrono::microseconds>(fill_table_end_time - program_start_time);
     auto program_duration = std::chrono::duration_cast<std::chrono::microseconds>(program_end_time - program_start_time);
 
-    std::cout << "table_fill_time " << fill_table_duration.count() << std::endl;
-    std::cout << "program_time " << program_duration.count() << std::endl;
+    std::cout << "table_fill_time " << (double)fill_table_duration.count()/1000000 << " [s]" << std::endl;
+    std::cout << "program_time " << (double)program_duration.count()/1000000 << " [s]" << std::endl;
 
     return 0;
  }
